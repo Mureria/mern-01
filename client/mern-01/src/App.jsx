78b5d924e39a2f3f62c15752f16bc34dc97413f7
Kevin Ireri <chosenireri42@gmail.com>
2023-11-06 02:07:50 +0300

    client ui

---
+++ client/mern-01/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CreateCustomer from './components/createCustomer'
+
+
+const App = () => {
+
+  return (
+    <>
+         <CreateCustomer/> 
+    </>
+  )
+}
+
+export default App
